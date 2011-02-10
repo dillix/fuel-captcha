a2fa8c8ea98221b1b8d0d6b262ada3254cbc38d8
Mikhail Khasaya <director@dillix.ru>
2011-02-10 11:48:23 +0300

    Add class skeleton and fonts.

---
+++ bootstrap.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Fuel Captcha
+ *
+ * A completely fresh Captcha for Fuel.
+ *
+ * @package		Fuel Captcha
+ * @version		alpha
+ * @author		Mikhail Khasaya
+ * @license		MIT License
+ * @copyright	2010 - 2011 Mikhail Khasaya
+ */
+
+Fuel\Core\Autoloader::add_namespace('\\Captcha', PKGPATH.'captcha'.DS.'classes');
+
+/* End of file bootstrap.php */
